279dca7efcc8856db71178a64d2fe76701bc5494
Daniel <dwgrossberg@gmail.com>
2024-11-01 13:34:40 -0400

    Change duplication verification to server side instead of db check

--- pymongo_db.py
+++ pymongo_db.py
@@ -11,6 +11,7 @@ load_dotenv(dotenv_path)
 class PyMongo_DB:
     def __init__(self):
         self.CONNECTION_STRING = os.environ.get("MONGODB_URI")
+        self.internship_set = set()
 
     def get_database(self):
         client = MongoClient(self.CONNECTION_STRING)
@@ -19,27 +20,33 @@ class PyMongo_DB:
     def insert_docs(self, internships):
         db = self.get_database()
         collection = db["internships-2025"]
+        data = []
 
         for internship in internships:
-            data = {
-                "company": internship[0],
-                "position": internship[1],
-                "link": internship[3],
-                "location": internship[2],
-                "date-posted": internship[4],
-                "applied": False,
-                "online-assessment": False,
-                "date-applied": None,
-                "interview-round": None,
-                "phone-screen": False,
-                "referral": False,
-                "result": None,
-            }
-            if collection.count_documents({
-                "company": internship[0],
-                "position": internship[1],
-                "link": internship[3],
-                "location": internship[2]
-            }, limit=1):
+            test_tuple = (
+                internship[0],
+                internship[1],
+                internship[3],
+                internship[2]
+                )
+            if test_tuple in self.internship_set:
                 continue
-            collection.insert_one(data)
+            else:
+                self.internship_set.add(test_tuple)
+                listing = {
+                    "company": internship[0],
+                    "position": internship[1],
+                    "link": internship[3],
+                    "location": internship[2],
+                    "date-posted": internship[4],
+                    "applied": False,
+                    "online-assessment": False,
+                    "date-applied": None,
+                    "interview-round": None,
+                    "phone-screen": False,
+                    "referral": False,
+                    "result": None,
+                }
+                data.append(listing)
+
+        collection.insert_many(data)
